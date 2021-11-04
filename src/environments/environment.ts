b27dd7928c8215a85a87708a491a3da7e2440a7b
ADAUTO DOS LUCAS <adautosl.lucas@gmail.com>
2021-11-04 14:36:47 -0300

    criado o marvel-routing.module, para configurar as chamadas das telas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'https://gateway.marvel.com:443/v1/public/characters?apikey=f7ae8ad508e93f0d9abe1502d0755727'
 };
 
 /*
