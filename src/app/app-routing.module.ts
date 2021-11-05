c6d1285f119310e66af71bb37149b9b3ed5f7933
ADAUTO DOS LUCAS <adautosl.lucas@gmail.com>
2021-11-05 10:22:12 -0300

    adicionado a rota marvel-cosmics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: '/home',
-  },{
+  }, {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  },{
+  }, {
     path: 'marvel-character',
     loadChildren: () => import('./marvel-character/marvel-character.module').then(m => m.MarvelCharacterModule)
+  },{
+    path: 'marvel-cosmics',
+    loadChildren: () => import('./marvel-cosmics/marvel-cosmics.module').then(m => m.MarvelCosmicsModule)
   }
 
 ];
