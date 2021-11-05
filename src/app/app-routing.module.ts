eed062ca84f28afb95a963bc9b5bd52a0c719e36
ADAUTO DOS LUCAS <adautosl.lucas@gmail.com>
2021-11-05 08:50:57 -0300

    adicionado o roteamento de marvel-character

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },{
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },{
+    path: 'marvel-character',
+    loadChildren: () => import('./marvel-character/marvel-character.module').then(m => m.MarvelCharacterModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
